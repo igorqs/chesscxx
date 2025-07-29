5b38cb79015bd73c9975614114bc418f8df6ed35
igorqs <igorqs@gmail.com>
2025-07-29 20:42:31 -0300

    fix test job

--- cmake/dev-mode.cmake
+++ cmake/dev-mode.cmake
@@ -5,8 +5,12 @@ if(BUILD_TESTING)
   add_subdirectory(tests)
 endif()
 
+option(BUILD_DOCS "Build documentation using Doxygen+Breathe+Sphinx" OFF)
+if(BUILD_DOCS)
+  include(cmake/docs-targets.cmake)
+endif()
+
 include(cmake/iwyu-targets.cmake)
-include(cmake/docs-targets.cmake)
 include(cmake/lint-targets.cmake)
 include(cmake/spell-targets.cmake)
 
