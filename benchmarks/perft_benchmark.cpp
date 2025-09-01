29dd3a9a9f015b817b7358ec0040aaf839d5062a
igorqs <igorqs@gmail.com>
2025-09-01 09:49:47 -0300

    build: add benchmark targets

--- benchmarks/perft_benchmark.cpp
+++ benchmarks/perft_benchmark.cpp
@@ -24,7 +24,7 @@ auto Perft(chesscxx::Game& game, int64_t depth) -> int64_t {
 template <class... Args>
 void BM_Perft(benchmark::State& state, Args&&... args) {
   auto args_tuple = std::make_tuple(std::forward<Args>(args)...);
-  auto [fen, depth] = args_tuple;
+  auto [fen, depth, expected_result] = args_tuple;
 
   auto parsed_game =
       chesscxx::parse<chesscxx::Game>(fen, chesscxx::parse_as::Fen{});
@@ -32,38 +32,43 @@ void BM_Perft(benchmark::State& state, Args&&... args) {
   if (!parsed_game) return;
 
   for ([[maybe_unused]] auto ignore : state) {
-    Perft(parsed_game.value(), depth);
+    if (Perft(parsed_game.value(), depth) != expected_result) {
+      std::abort();
+    }
   }
 }
 }  // namespace
 
 BENCHMARK_CAPTURE(BM_Perft, position_1,
-                  "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 4)
+                  "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 4,
+                  197281)
     ->Unit(benchmark::kMillisecond);
 
 BENCHMARK_CAPTURE(
     BM_Perft, position_2,
-    "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 3)
+    "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 3,
+    97862)
     ->Unit(benchmark::kMillisecond);
 
 BENCHMARK_CAPTURE(BM_Perft, position_3,
-                  "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 4)
+                  "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 4, 43238)
     ->Unit(benchmark::kMillisecond);
 
 BENCHMARK_CAPTURE(
     BM_Perft, position_4,
-    "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 4)
+    "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 4,
+    422333)
     ->Unit(benchmark::kMillisecond);
 
 BENCHMARK_CAPTURE(BM_Perft, position_5,
                   "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
-                  3)
+                  3, 62379)
     ->Unit(benchmark::kMillisecond);
 
 BENCHMARK_CAPTURE(
     BM_Perft, position_6,
     "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
-    3)
+    3, 89890)
     ->Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN();
