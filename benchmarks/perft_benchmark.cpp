f6f3fb0fd4fe27b52cf11435346df3db7b7d49c0
igorqs <igorqs@gmail.com>
2025-09-01 11:14:00 -0300

    build: add benchmark targets

--- benchmarks/perft_benchmark.cpp
+++ benchmarks/perft_benchmark.cpp
@@ -48,7 +48,7 @@ BENCHMARK_CAPTURE(BM_Perft, position_1,
 BENCHMARK_CAPTURE(
     BM_Perft, position_2,
     "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 3,
-    97861)
+    97862)
     ->Unit(benchmark::kMillisecond);
 
 BENCHMARK_CAPTURE(BM_Perft, position_3,
